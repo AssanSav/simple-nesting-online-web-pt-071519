211abb99b21c22eae9cbba06f29e3cc4d7173153
assane savadogo <assane.savadogo81@gmail.com>
2019-07-27 15:29:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,10 +59,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
- :yukihiro_matsumoto => {
-   :known_for => "Ruby",
-   :languages => ["LISP", "C"] }
-
+ :yukihiro_matsumoto => 
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -78,7 +75,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash[:yukihiro_matsumoto] = 
+     programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"] }
+
 end
 
 def changing_alan
