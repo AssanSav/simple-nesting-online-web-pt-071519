3ff601bfbacdb443708aff54b0b72a11f594d778
assane savadogo <assane.savadogo81@gmail.com>
2019-07-27 15:31:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -95,11 +95,8 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+     } 
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
